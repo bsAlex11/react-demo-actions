515bcb295873a61dfd3ec29e3b883e633c5bd1c6
bsAlex11 <baies.alexandru90@yahoo.ro>
2025-07-03 12:20:42 +0300

    security script injection

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -6,6 +6,16 @@ const exec = require('@actions/exec');
 
 function run() {
   core.notice('eloo from action');
+
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  const s3Uri = `s3://${bucket}`;
+  core.exec(`aws s3 sync ${distFolder} ${s3Uri}} --region ${bucketRegion}`);
+
+  const webUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput('web-url', webUrl);
 }
 
 run();
